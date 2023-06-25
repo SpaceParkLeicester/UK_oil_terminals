dc79708065ceb925c9044bd7c86c89500a9323e7
vrym2 <vardhan609@gmail.com>
2023-06-25 20:03:59 +0000

    update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,15 +5,7 @@ def test_uk_oil_data():
     """Testing UK oil terminal xlsx file"""
     data = OilTerminals()
     locations = data.location_names()
+    data.location_wkt()
     locations_geojson = data.geojson_data()
     assert locations is not None
     assert locations_geojson is not None
-    print(locations_geojson)
-
-def test_xlsx_csv():
-    """Testing xlsx to csv conversion"""
-    file_path = "data/uk_oil_terminals.xlsx"
-    csv_file_name = "uk_oil_terminals.csv"
-    data = OilTerminals()
-    path = data.xlsx_to_csv(file_path,csv_file_name)
-    assert os.path.exists(path)
